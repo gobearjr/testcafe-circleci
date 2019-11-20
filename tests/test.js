4c935af266f3f0bc591c4244af3040774533e2bb
Yoshiki Kitamura <y-kitamura@pro-seeds.co.jp>
2019-11-20 14:39:36 +0900

    testcafe公式のcircle ciのdemoファイルにcheck.jsを適用

--- tests/test.js
+++ tests/test.js
@@ -1,106 +1,38 @@
-import Page from './page-model';
+import { Selector, RequestLogger } from 'testcafe';
 
-fixture `A set of examples that illustrate how to use TestCafe API`
-    .page `https://devexpress.github.io/testcafe/example/`;
+const LoginID = 'testcafe_zentai';
+const LoginPass = 'testcafe_zentai';
+const logger = RequestLogger('https://dev3.learning-ware.jp/');
 
-// Page model
-const page = new Page();
+fixture `check_200`
+    .page `https://dev3.learning-ware.jp/`;
 
-// Tests
-test('Text typing basics', async t => {
-    await t
-        .typeText(page.nameInput, 'Peter')                      // Type name
-        .typeText(page.nameInput, 'Paker', { replace: true })   // Replace with last name
-        .typeText(page.nameInput, 'r', { caretPos: 2 })         // Correct last name
-        .expect(page.nameInput.value).eql('Parker');            // Check result
-});
+test
+    .requestHooks(logger)
+    ('login', async t => {
 
-
-test('Click an array of labels and then check their states', async t => {
-    for (const feature of page.featureList) {
         await t
-            .click(feature.label)
-            .expect(feature.checkbox.checked).ok();
-    }
-});
+            .click(Selector('#loginid'))
+            .typeText(Selector('#loginid'), LoginID)
+            .pressKey('tab')
+            .typeText(Selector('#password'), LoginPass)
+            .pressKey('enter');
 
+        await t
+            .expect(logger.contains(record => record.response.statusCode === 200)).ok()
 
-test('Dealing with text using keyboard', async t => {
-    await t
-        .typeText(page.nameInput, 'Peter Parker')           // Type name
-        .click(page.nameInput, { caretPos: 5 })             // Move caret position
-        .pressKey('backspace')                              // Erase a character
-        .expect(page.nameInput.value).eql('Pete Parker')    // Check result
-        .pressKey('home right . delete delete delete')      // Pick even shorter form for name
-        .expect(page.nameInput.value).eql('P. Parker');     // Check result
-});
+            //ユーザーの登録
+            .navigateTo('https://dev3.learning-ware.jp/admin/user/create')
+            .expect(logger.contains(record => record.response.statusCode === 200)).ok()
 
+            //ユーザーの一括登録・変更(CSV)
+            .navigateTo('https://dev3.learning-ware.jp/admin/user-csv')
+            .expect(logger.contains(record => record.response.statusCode === 200)).ok()
 
-test('Moving the slider', async t => {
-    const initialOffset = await page.slider.handle.offsetLeft;
+            //ユーザーの管理
+            .navigateTo('https://dev3.learning-ware.jp/admin/user/list')
+            .expect(logger.contains(record => record.response.statusCode === 200)).ok();
 
-    await t
-        .click(page.triedTestCafeCheckbox)
-        .dragToElement(page.slider.handle, page.slider.tick.withText('9'))
-        .expect(page.slider.handle.offsetLeft).gt(initialOffset);
-});
+        const logRecord = logger.requests[0];
 
-
-test('Dealing with text using selection', async t => {
-    await t
-        .typeText(page.nameInput, 'Test Cafe')
-        .selectText(page.nameInput, 7, 1)
-        .pressKey('delete')
-        .expect(page.nameInput.value).eql('Tfe');   // Check result
-});
-
-
-test('Handle native confirmation dialog', async t => {
-    await t
-        .setNativeDialogHandler(() => true)
-        .click(page.populateButton);
-
-    const dialogHistory = await t.getNativeDialogHistory();
-
-    await t.expect(dialogHistory[0].text).eql('Reset information before proceeding?');
-
-    await t
-        .click(page.submitButton)
-        .expect(page.results.innerText).contains('Peter Parker');
-});
-
-
-test('Pick option from select', async t => {
-    await t
-        .click(page.interfaceSelect)
-        .click(page.interfaceSelectOption.withText('Both'))
-        .expect(page.interfaceSelect.value).eql('Both');
-});
-
-
-test('Filling a form', async t => {
-    // Fill some basic fields
-    await t
-        .typeText(page.nameInput, 'Bruce Wayne')
-        .click(page.macOSRadioButton)
-        .click(page.triedTestCafeCheckbox);
-
-    // Let's leave a comment...
-    await t
-        .typeText(page.commentsTextArea, "It's...")
-        .wait(500)
-        .typeText(page.commentsTextArea, '\ngood');
-
-    // I guess, I've changed my mind
-    await t
-        .wait(500)
-        .selectTextAreaContent(page.commentsTextArea, 1, 0)
-        .pressKey('delete')
-        .typeText(page.commentsTextArea, 'awesome!!!');
-
-    // Let's submit our form
-    await t
-        .wait(500)
-        .click(page.submitButton)
-        .expect(page.results.innerText).contains('Bruce Wayne');
-});
+    });
